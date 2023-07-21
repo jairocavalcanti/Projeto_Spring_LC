e1bbda7ec5b51c587cc0e9145fea18d6b4c77353
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-21 06:00:40 -0300

    Mapeamento com @PathVariable

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -2,6 +2,7 @@ package com.example.carros.api;
 
 //import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,11 +40,21 @@ public class IndexController {
     }
     
     
-    @GetMapping("/login")
-     public String login(@RequestParam("login") String login, @RequestParam("senha") String senha){
+    @GetMapping("/login/{login}/senha/{senha}")
+     public String login(@PathVariable("login") String login, @PathVariable("senha") String senha){
         return "Login: " + login + ", Senha: " + senha;
      }
  
+     @GetMapping("/carros/{id}")
+     public String getcarroById(@PathVariable("id")Long id){
+        return "Carro by id: " + id;
+     }
+
+     
+     @GetMapping("/carros/tipo/{tipo}")
+     public String getcarroByTipo(@PathVariable("tipo")String tipo){
+        return "Lista Carros: " + tipo;
+     }
 
  
     /* 
