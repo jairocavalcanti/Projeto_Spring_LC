42df020f9bff2ba97475f72ab1092d109b581b68
Jairo <jairoregiscavalcanti@gmail.com>
2023-10-13 19:07:53 -0300

    Metodo_update_mapeado

--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,14 @@ public class CarrosController {
       }
 
       /* !! ALTERAÇÕES DE DADOS PODEM SER FEITAS PASSANDO O ID COMO PARAMETRO ATRAVES DO METODO "post" DA APLICAÇÃO !! */
+
+      @PutMapping("{id}")
+      public String put(@PathVariable("id") Long id, @RequestBody Carro carro){
+
+         Carro c = service.update(carro, id);
+
+         return "Carro atualizado com sucesso: " + c.getId();
+      }
 
 
    }
